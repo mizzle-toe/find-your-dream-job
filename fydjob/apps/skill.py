81641c42aeadf7b67a0049397c76070a54faf13a
Jasmin Kazi <jasmin_kazi@outlook.com>
2021-03-09 16:33:03 +0100

    switch of data sources and improvements on the app

--- fydjob/apps/skill.py
+++ fydjob/apps/skill.py
@@ -3,31 +3,34 @@ import altair as alt
 import pandas as pd
 from gensim.models import Word2Vec
 from fydjob.Word2VecPipeline import WordPipeline
+from fydjob.utils import category_tagger
+from fydjob.NLPFrame import NLPFrame
+import requests
 
 
 def app():
+    # Model
+    # get data from
+    #api
+    #url = 'http://0.0.0.0:3000'
+    #response = requests.get(url)
+    #response.json()
+
     #sidebar 
-    skill = st.sidebar.text_input('Search skill','python')
-    word = [skill]
-    #add number of skills and pass new model from alex!!!
-    no_skill = st.sidebar.number_input('n-closest skills',10)
+    skills = st.sidebar.text_input('Search skill','python')
+    #word = [skills.split(',')]
+    no_skill = st.sidebar.number_input('n-closest skills',5)
     
     #search for skill
     st.markdown("""
     ### Find the closest skills for:""")
-    st.write(skill)
-
-    # Model
-    # replace with API
-    
-    # old model
-    w2v_model = Word2Vec.load("/Users/jasminkazi/code/mizzle-toe/find-your-dream-job/fydjob/data/models/w2v_model_baseline.model")
-    df_words= pd.DataFrame(w2v_model.wv.most_similar(word), columns=['Similar word', 'distance']).assign(hack='').set_index('hack') # works with single words
+    st.write(skills)
     
     #new model with n-recommendation
-    #w2v_model = WordPipeline("/Users/jasminkazi/code/mizzle-toe/find-your-dream-job/fydjob/data/models/w2v_model_baseline.model")
-    #df_words= pd.DataFrame(w2v_model.most_similar_skills(skill,n_recommendations= no_skill)).assign(hack='').set_index('hack')
-    
+    w2v_model = WordPipeline("/Users/jasminkazi/code/mizzle-toe/find-your-dream-job/fydjob/data/models/w2v_model_baseline.model")
+    #df_words= pd.DataFrame(w2v_model.most_similar_skills(skills,n_recommendations= no_skill),columns=['List of similar words']).assign(hack='').set_index('hack')
+    df_words = pd.DataFrame(category_tagger(w2v_model.most_similar_skills(skills,n_recommendations= no_skill)),columns=['List of similar words','Skill category']).assign(hack='').set_index('hack')
+
     #table
     st.write(df_words)
   
