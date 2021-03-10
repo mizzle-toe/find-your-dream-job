34c2fb2efc946048f53ac7b8a8ff366cb3b79597
Jasmin Kazi <jasmin_kazi@outlook.com>
2021-03-10 12:32:05 +0100

    implemented skill count from fydjob.utils

--- fydjob/apps/summary.py
+++ fydjob/apps/summary.py
@@ -19,9 +19,7 @@ def app():
 
     #local data
     data = NLPFrame().df
-    #data = joblib.load('/Users/jasminkazi/code/mizzle-toe/find-your-dream-job/fydjob/output/indeed_proc/processed_data.joblib')
-    data = data.drop(columns=['job_info_tokenized','job_text_tokenized_titlecase', 'job_title_tokenized','job_text_tokenized'])
-    #data = data.sample(n=2000)
+    data = data.drop(columns=['job_info_tokenized','job_text_tokenized_titlecase','job_text_tokenized'])
 
     #Sidebar
     # Title
@@ -49,7 +47,7 @@ def app():
     
 
     # Numerical KPIs
-    no_jd = str(data['job_title'].count())
+    no_jd = str(data['job_id'].count())
     st.markdown("""
     ### Number of available jobs:""")
     st.write(no_jd)
@@ -141,63 +139,32 @@ def app():
     # most in demand skills from the dictionary
     st.markdown("""
     ### Most in demand skills:""")
-    # get the aggregated values for the skills in all JDs
-    @st.cache(allow_output_mutation=True)
-    def get_skill_aggr(all_vacancies):
-        categories = utils.load_skills()
-        vacancy_set = set(all_vacancies)
-        matching_skill_per_category = {}
-
-        for category in categories:
-            matching_skill_per_category[str(category)]=vacancy_set.intersection(categories[category])
-        
-        occ = {}
-        for category in categories:
-            occ[category] = {}
-            for s in matching_skill_per_category[str(category)]:
-                occ[category][s] =  all_vacancies.count(s)
-        return occ
-
-    #create the skill set for all jobs and count the occurences
-    all_vacancies=[]
-    for job in data['job_text_tokenized_processed']:
-        #print(len(job))
-        all_vacancies = all_vacancies + job
-    total_occurences = get_skill_aggr(all_vacancies)
-
-    # put the result into a data frame
-    df = pd.DataFrame(total_occurences).reset_index()
-    df= df.rename(columns = {'index':'skill'})
-    #transform the dataframes -> put the skill category in one column
-    df_bus = df[['skill','business']].dropna().sort_values('business', ascending = False).rename(columns = {'business':'count'})
-    df_bus['category']='business'
-    df_know = df[['skill','knowledge']].dropna().sort_values('knowledge', ascending = False).rename(columns = {'knowledge':'count'})
-    df_know['category']='knowledge'
-    df_code = df[['skill','programming']].dropna().sort_values('programming', ascending = False).rename(columns = {'programming':'count'})
-    df_code['category']='programming'
-    df_soft = df[['skill','soft_skills']].dropna().sort_values('soft_skills', ascending = False).rename(columns = {'soft_skills':'count'})
-    df_soft['category']='soft skills'
-    #make all the sub data frames into one new dataframe
-    df_categ = df_bus.append(df_know).append(df_code).append(df_soft)
-
-
-    @st.cache(allow_output_mutation=True)
-    def get_skills(df_categ):
-        #source = df_categ
-        source = df_categ.nlargest(50, 'count')
-        categs = np.array(df_categ['category'].unique())
+    # skill count from utils:
+    
+    def get_skill_count():
+        count = utils.count_skills(data['skills'])
+        df_skills = pd.DataFrame()
+        for key in count.keys():
+            df_key=pd.DataFrame.from_dict(count[key])
+            df_key['Category']=key
+            df_skills = df_skills.append(df_key)
+        source =df_skills.rename(columns = {0:'Skill',1:'Count'}).nlargest(50, columns='Count')
+        #isolate the categories for the dropbox
+        categs = np.array(df_skills['Category'].unique())
         category = np.insert(categs, 0, 'all')
+
         #Dropdownbox
         input_dropdown = alt.binding_select(options = category)
-        selection = alt.selection_single(fields = ['category'], bind = input_dropdown, name= 'Skill ')
+        selection = alt.selection_single(fields = ['Category'], bind = input_dropdown, name= 'Skill ')
 
-        c_skills= alt.Chart(source).mark_bar().encode(
-        x = ('count:Q'),
-        y = alt.Y('skill',sort='-x')).add_selection(selection).transform_filter(selection)
-        
-        return c_skills
-    c_skills = get_skills(df_categ)
-    st.write(c_skills)
-
-
-  
+        c_skills_count= alt.Chart(source).mark_bar().encode(
+            x = ('Count:Q'),
+            y = alt.Y('Skill',sort='-x')).add_selection(selection).transform_filter(selection)
+            # y = alt.Y('Skill',sort='-x')
+            # ).transform_window(
+            #     rank = 'rank(Skill)'
+            #     ).add_selection(selection
+            #     ).transform_filter((selection)&(alt.datum.rank < 30))
+        return c_skills_count
+    c_skills_count = get_skill_count()
+    st.write(c_skills_count)
