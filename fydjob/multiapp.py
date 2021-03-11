32c279c17bb31abd85713c464ae1086abb65f764
Vlad Gheorghe <contact@vlad-gheorghe.com>
2021-03-11 19:52:14 +0100

    docker

--- fydjob/multiapp.py
+++ fydjob/multiapp.py
@@ -22,7 +22,7 @@ class MultiApp:
         app.run()
     """
     def __init__(self):
-        self.API_URL = "http://0.0.0.0:3000"
+        self.API_URL = "http://0.0.0.0:8000"
         self.apps = []
 
     def add_app(self, title, func):
